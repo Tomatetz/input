d7f7dffb728157d96b2ffcf4344e1e2501faa103
Tomatetz <tomatetz@gmail.com>
2015-05-19 16:36:13 +0300

    asd

--- scripts/app.js
+++ scripts/app.js
@@ -7,11 +7,12 @@
 
 
 
+
     $.each(users, function(i, user){
         user.full_name = user.user_name +' '+ user.user_surname;
         $('.users').append('<div class="user" data-id="'+ user.user_id +'" onclick=""><div class="user_inner">' +
         '<div class="img_wrapper"><img class="user_img" src="'+ user.user_pic +'"></div>' +
-        '<div class="user_name">'+ user.user_name +' '+ user.user_surname +'</div><div class="wddi_sub"></div></div></div>')
+        '<div class="user_name">'+ user.full_name +'</div><div class="wddi_sub"></div></div></div>')
     });
     function focus(){
         $('.users_list').css('display','block');
@@ -21,26 +22,57 @@
     function inputChange(a,b){
         //$('.users_list').css('display','block');
         //$('.user').each(function(){console.log($(this).data());})
-        console.log(event.keyCode);
+        //console.log(x.value);
         translit(x.value)
     }
     function translit(val){
+        /*$.each(users, function(i, user){
+            if(user.full_name.indexOf(val)>-1 ){
+
+            }
+        })*/
+        $('.user_name').each(function(){
+            var thisHtml = $(this).html()
+            console.log(thisHtml);
+            if(thisHtml.indexOf(val)==-1){
+                $(this).parent().parent().css('display', 'none')
+            }
+        })
         var eng_rus={
             'q':'й',
-            'w':'w',
-            'e':'e',
-            'r':'й',
-            't':'й',
-            'y':'й',
-            'u':'й',
-            'i':'й',
-            'o':'й',
-            'p':'й',
-            '[':'й',
-            ']':'й',
-            'a':'й',
-            's':'й'
-
+            'w':'ц',
+            'e':'у',
+            'r':'к',
+            't':'е',
+            'y':'н',
+            'u':'г',
+            'i':'ш',
+            'o':'щ',
+            'p':'з',
+            '[':'х',
+            ']':'ъ',
+            'a':'ф',
+            's':'ы',
+            'd':'в',
+            'f':'а',
+            'g':'п',
+            'h':'р',
+            'j':'о',
+            'k':'л',
+            'l':'д',
+            ';':'ж',
+            '\'':'э',
+            'z':'я',
+            'x':'ч',
+            'c':'с',
+            'v':'м',
+            'b':'и',
+            'n':'т',
+            'm':'ь',
+            ',':'б',
+            '.':'ю'
         }
+        var eq = eng_rus[val.slice(-1)];
+        console.log(!!eq);
     }
 })()
--- scripts/test.js
+++ scripts/test.js
@@ -1,8 +1,8 @@
 
-function inputChange(a,b){
+/*function inputChange(event){
     var x = document.getElementById("form_input");
     //$('.users_list').css('display','block');
     //$('.user').each(function(){console.log($(this).data());})
-    //console.log(event.keyCode);
+    console.log(event.keyCode);
     console.log(x.value);
-}
+}*/
